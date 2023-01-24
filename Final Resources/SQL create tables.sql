2a22fb7255155648729737fbeba027bddfa5f068
Ccasti13 <109004259+Ccasti13@users.noreply.github.com>
2023-01-23 19:58:54 -0700

    Add files via upload

---
+++ Final Resources/SQL create tables.sql
@@ -0,0 +1,14 @@
+create table life_death (
+Countries varchar,
+	life_expectancy_men int,
+	life_expectancy_women int,
+	suicide_rate int
+
+)
+
+create table GDP_Happiness (
+Countries varchar,
+	GDP_Per_Captia int,
+	Happiness int
+
+)
